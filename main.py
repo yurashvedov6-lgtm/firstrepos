08fae5ea0e2d85c215292d4665969302612ca9dd
yurashvedov <yurashvedov6@gmail.com>
2026-04-08 09:25:57 +0200

    add1 main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.uix.button import Button
 class SimplApp(App):
     def build(self):
-        return Button(text="PUT ME")
+        return Button(text="PUT ME and hello world")
 
 
 
